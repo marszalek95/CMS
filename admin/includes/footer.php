0bf82e45f405c0149fc13144cecd706efc6e318c
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-08 22:35:25 +0100

    Repaired scrollbar bug

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,30 +7,23 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
     
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Number'],
-          ['Views',    <?php echo $session->count; ?>],
-          ['Users',    <?php echo User::count_records(); ?>],
-          ['Photos',   <?php echo Photo::count_records(); ?>],
-          ['Comments', <?php echo Comment::count_records(); ?>],
-        ]);
-
-        var options = {
-          title: 'My Daily Activities'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
+   
  
+
+    <div class="container-fluid">
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <font color="white"><center><p>Copyright &copy; Adam Marszalek 2019</p></center></font>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
 
 
 </body>
