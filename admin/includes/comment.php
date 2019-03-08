060acf213eb8068a157af1f5f9ea15beb8ce388e
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-08 22:33:43 +0100

    Added notifications methods

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -5,11 +5,12 @@
 class Comment extends Db_object
 {
     protected static $db_table = "comments";
-    protected static $db_table_fields = ['photo_id', 'author', 'body'];
+    protected static $db_table_fields = ['photo_id', 'author', 'body', 'status'];
     public $id;
     public $photo_id;
     public $author;
     public $body;
+    public $status;
 
 
     
@@ -19,6 +20,21 @@ class Comment extends Db_object
         
         return self::find_this_query($sql);
     }
+    
+    public static function find_new_comments()
+    {
+        $sql = "SELECT * FROM " . self::$db_table . " WHERE status = 0";
+        
+        return self::find_this_query($sql);
+    }
+    
+    public function updade_status($id)
+    {
+        global $database;
+        $sql = "UPDATE " . static::$db_table . " SET status = 1 WHERE id=$id";
+        $database->query($sql);
+        return true;
+    }
 
     public function delete_comment($photo_id)
     {
@@ -26,7 +42,7 @@ class Comment extends Db_object
         
         $id = $database->escape_string($this->id);
         
-        $sql = "DELETE  FROM " . static::$db_table . " WHERE photo_id=$photo_id";
+        $sql = "DELETE * FROM " . static::$db_table . " WHERE photo_id=$photo_id";
         
         $database->query($sql);
         
