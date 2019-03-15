c980b3d0aaf42ba5d4853a773383b286889dd1ce
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-15 22:45:39 +0100

    Added date variable

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -5,12 +5,14 @@
 class Comment extends Db_object
 {
     protected static $db_table = "comments";
-    protected static $db_table_fields = ['photo_id', 'author', 'body', 'status'];
+    protected static $db_table_fields = ['photo_id', 'user_id', 'author', 'body', 'status', 'date'];
     public $id;
     public $photo_id;
+    public $user_id;
     public $author;
     public $body;
     public $status;
+    public $date;
 
 
     
@@ -21,9 +23,16 @@ class Comment extends Db_object
         return self::find_this_query($sql);
     }
     
-    public static function find_new_comments()
+    public static function find_all_comments_pagination($photo_id, $items_per_page, $offset)
     {
-        $sql = "SELECT * FROM " . self::$db_table . " WHERE status = 0";
+        $sql = "SELECT * FROM " . self::$db_table . " WHERE photo_id = " . $photo_id . " ORDER BY photo_id ASC LIMIT {$items_per_page} OFFSET {$offset}";
+        
+        return self::find_this_query($sql);
+    }
+    
+    public static function find_new_comments($user_id)
+    {
+        $sql = "SELECT * FROM " . self::$db_table . " WHERE status = 0 && user_id = {$user_id}";
         
         return self::find_this_query($sql);
     }
@@ -40,9 +49,8 @@ class Comment extends Db_object
     {
         global $database;
         
-        $id = $database->escape_string($this->id);
         
-        $sql = "DELETE * FROM " . static::$db_table . " WHERE photo_id=$photo_id";
+        $sql = "DELETE FROM " . static::$db_table . " WHERE photo_id=$photo_id";
         
         $database->query($sql);
         
