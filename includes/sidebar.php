fb1360b4f45871a6eb06cd5ba05cffc8905123ce
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-15 22:27:55 +0100

    Added photo search

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,50 +3,20 @@
             <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+                    <form action="search.php" method="get">
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input type="text" name="query" class="form-control">                     
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
+                            <button class="btn btn-default" type="submit">
+                                <a class="glyphicon glyphicon-search"></a>
                         </button>
                         </span>
                     </div>
+                    </form>
                     <!-- /.input-group -->
                 </div>
 
-                <!-- Blog Categories Well -->
-        <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
+               
 
                 <!-- Side Widget Well -->
                 <div class="well">
