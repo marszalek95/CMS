1f3ef9c8a4e2072b2387f034212b6929f7a842a6
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-15 22:19:51 +0100

    First commit

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php include("includes/header.php"); ?>
+
+<div class="container">
+    
+    <div class="row">
+        
+        <p>This is simple CMS Photo Gallery site.</p>
+        
+    </div>
+    
+</div>
+
+
+
+
+     
+
+<?php include("includes/footer.php"); ?>
