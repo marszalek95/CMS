f885faf266ebd55027377111f03cec785111b87e
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-15 22:35:53 +0100

    Some improvements

--- includes/footer.php
+++ includes/footer.php
@@ -19,7 +19,7 @@
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 
 </body>
 
