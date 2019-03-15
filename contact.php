12e086bddc7b6e1cba573d3a4ed487137e4ed7af
Adam Marszalek <adam.marszalek95@gmail.com>
2019-03-15 22:15:48 +0100

    First commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php include("includes/header.php"); ?>
+
+<div class="container">
+    
+    <div class="row">
+        
+        <p>Adam Marszałek</p>
+        <p>e-mail: adam.marszalek95@gmail.com 
+        
+    </div>
+    
+</div>
+
+
+
+
+     
+
+<?php include("includes/footer.php"); ?>
